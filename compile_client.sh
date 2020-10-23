1d740ad642af912e444379f875c747f7f8d48bd0
zhenyuanzi <zhenyuanzi@oradar.com.cn>
2020-10-23 17:37:19 +0800

    rm compile script

--- compile_client.sh
+++
@@ -1,5 +0,0 @@
-#! /bin/sh
-gcc client.c -g -o client.out \
--I /usr/include -L /usr/lib/x86_64-linux-gnu -lzmq -lczmq
-gcc server.c -g -o server.out \
--I /usr/include -L /usr/lib/x86_64-linux-gnu -lzmq -lczmq
